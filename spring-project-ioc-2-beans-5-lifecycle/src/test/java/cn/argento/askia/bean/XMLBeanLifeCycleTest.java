d59611e9827c21f520cdf5f5ce222a3f84140cfd
ArgentoAskia <askiaxiaoming@outlook.com>
2024-11-30 22:01:10 +0800

    2024.11.30 基于XML配置容器Bean Lifestyle测试类

---
+++ spring-project-ioc-2-beans-5-lifecycle/src/test/java/cn/argento/askia/bean/XMLBeanLifeCycleTest.java
@@ -0,0 +1,44 @@
+package cn.argento.askia.bean;
+
+import cn.argentoaskia.bean.User3;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
+
+public class XMLBeanLifeCycleTest {
+
+    private ClassPathXmlApplicationContext applicationContext;
+
+    @Before
+    public void init(){
+        System.out.println("## 依赖注入完成之后，开始进行生命周期回调");
+        System.out.println();
+        applicationContext = new ClassPathXmlApplicationContext("/spring-context.xml");
+        System.out.println();
+    }
+
+    @After
+    public void destroy(){
+        System.out.println("## 准备关闭容器");
+        System.out.println();
+        System.out.println(Arrays.toString(applicationContext.getBeanDefinitionNames()));
+        applicationContext.close();
+        System.out.println();
+        System.out.println("## 容器已关闭");
+    }
+
+    @Test
+    public void testAllLifeCycle(){
+        User3 user3_1 = applicationContext.getBean("user3_1", User3.class);
+        User3 user3_2 = applicationContext.getBean("user3_2", User3.class);
+        User3 user3_3 = applicationContext.getBean("user3_3", User3.class);
+        System.out.println("## 输出bean");
+        System.out.println("user3_1: " + user3_1);
+        System.out.println("user3_2: " + user3_2);
+        System.out.println("user3_3: " + user3_3);
+        System.out.println();
+    }
+}
